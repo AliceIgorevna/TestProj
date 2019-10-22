28d6c390f8f6c3023765c6f083213684bf0d37b1
kamenska <nadiia.kamenska@finiata.com>
2019-10-22 18:58:19 +0200

    from origin

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,7 +2,9 @@
  * Created by Alisa_Demennikova on 8/23/2016.
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-    }
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+		System.out.println("How a great sunny day!");
+	}
 }
